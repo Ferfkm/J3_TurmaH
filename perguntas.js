36e55b8c31c2c06887923f18d1ac587b27d9488d
Fernanda Fernandes <98698976+Ferfkm@users.noreply.github.com>
2024-11-28 11:39:11 +0000

    CONCLUSAO

--- perguntas.js
+++ perguntas.js
@@ -1,26 +1,31 @@
 criaCartao(
     'Programação',
     'O é Python?',
+    'python.png',
     'O Python é uma linguagem de programação.'
 )
 criaCartao(
     'Geografia',
     'Qual a capital da França',
+    'globo-mundial.png',
     'A capital da França é Paris'
 )
 criaCartao(
     'Programação',
     'O que é uma função?',
+    'python.png',
     'Uma função é um bloco de código que executa alguma tarefa'
 )
 criaCartao(
     'Matemática',
     'O que é uma progressão aritmética (PA)?',
+    'python.png',
     'PA é uma sequência de termos em que a diferença entre um termo e o seu anterior é sempre constante.'
 )
 criaCartao(
     'Programação',
     'O é Python?',
+    'python.png',
     'O Python é uma linguagem de programação.'
 )
 criaCartao(
